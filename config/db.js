508843a58cce295f60c09411647aec83c3bfd69a
vishal <ivishalgautamm@gmail.com>
2023-10-07 12:19:59 +0530

    shifted

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: process.env.USER,
-  host: process.env.HOST,
+  host: process.env.JANGID_HOST,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  database: process.env.JANGID_DATABASE,
   port: process.env.DB_PORT,
 });
 
