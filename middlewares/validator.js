538c32f45c491cc0d42c14a7b65e34e00f43e58e
vishal <ivishalgautamm@gmail.com>
2023-10-09 12:58:36 +0530

    fixed some error

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -25,7 +25,6 @@ const validateWorker = [
   body("fullname")
     .isLength({ min: 3 })
     .withMessage("Name must be at least 3 characters long"),
-  body("email").isEmail().withMessage("Invalid email address"),
   body("phone")
     .isLength({ min: 10 })
     .withMessage("Phone number must be at least 10 characters long"),
