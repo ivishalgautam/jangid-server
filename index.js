50bcc5522e9c6b63afd24fb1c379a5794d135268
vishal <ivishalgautamm@gmail.com>
2023-10-10 11:18:46 +0530

    fixed some error

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const morgan = require("morgan");
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
