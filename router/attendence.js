780bb63276bf869600ba1e4a984704712ad3a21f
vishal <ivishalgautamm@gmail.com>
2023-10-11 18:33:08 +0530

    added worker dashboard details

--- router/attendence.js
+++ router/attendence.js
@@ -2,3 +2,5 @@ const router = require("express").Router();
 const Controller = require("../controller/attendence.controller");
 
 router.get("/", Controller.getWorkerAttendenceById);
+
+module.exports = router;
