aa9f59d9c84de5b2097a0fe442ebff1bb6e648ca
vishal <ivishalgautamm@gmail.com>
2023-10-12 17:38:00 +0530

    added check in check out

--- controller/attendence.controller.js
+++ controller/attendence.controller.js
@@ -44,6 +44,8 @@ async function createCheckOut(req, res) {
         ]
       );
     }
+
+    res.json({ message: "Logged out" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
