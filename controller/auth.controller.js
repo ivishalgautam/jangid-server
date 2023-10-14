779ce1fca817b5a7e8eab7deecd6e3e78acdd06e
vishal <ivishalgautamm@gmail.com>
2023-10-14 17:22:11 +0530

    added payouts api

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -157,7 +157,7 @@ async function workerlogin(req, res) {
         );
       }
 
-      res.json({ session_id: rows[0].uid, worker: worker.rows[0] });
+      res.json({ session_id: rows[0].uid });
     } else {
       console.error(`The point is outside ${radius} kilometers of the center.`);
       return res.status(400).json({ message: "You are out of radius!" });
