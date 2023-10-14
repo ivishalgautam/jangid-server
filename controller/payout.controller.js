d164314bbf758c19c4fadfeadc2b428377357f34
vishal <ivishalgautamm@gmail.com>
2023-10-14 10:49:05 +0530

    added payouts api

--- controller/payout.controller.js
+++ controller/payout.controller.js
@@ -29,7 +29,11 @@ async function addWorkerPayout(req, res) {
 
     await pool.query(
       `UPDATE workers SET total_paid = $1, pending_payout = $2 WHERE id = $3;`,
-      [total_paid + amount, total_payout - total_paid + amount, worker_id]
+      [
+        parseInt(total_paid) + parseInt(amount),
+        parseInt(total_payout) - parseInt(total_paid) + parseInt(amount),
+        parseInt(worker_id),
+      ]
     );
 
     res.json({ message: "Payout added" });
@@ -76,6 +80,54 @@ async function addSitePayout(req, res) {
   }
 }
 
+// get worker payout
+async function getWorkerPayouts(req, res) {
+  const { worker_id } = req.body;
+  try {
+    const worker = await pool.query(`SELECT * FROM workers WHERE id = $1`, [
+      worker_id,
+    ]);
+
+    if (worker.rowCount === 0) {
+      return res.status(404).json({ message: "Worker not found!" });
+    }
+
+    const { rows } = await pool.query(
+      `SELECT * FROM worker_payouts WHERE worker_id = $1;`,
+      [worker_id]
+    );
+    res.json(rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+}
+
+// get site payout
+async function getSitePayouts(req, res) {
+  const { site_id } = req.body;
+  try {
+    const site = await pool.query(`SELECT * FROM sites WHERE id = $1`, [
+      site_id,
+    ]);
+
+    if (site.rowCount === 0) {
+      return res.status(404).json({ message: "Site not found!" });
+    }
+
+    const { rows } = await pool.query(
+      `SELECT * FROM site_payouts WHERE site_id = $1;`,
+      [site_id]
+    );
+
+    res.json(rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+}
+
+// get all payouts with query site and worker
 async function getAllPayouts(req, res) {
   const { query } = req.body;
   try {
@@ -102,4 +154,10 @@ async function getAllPayouts(req, res) {
   }
 }
 
-module.exports = { addWorkerPayout, addSitePayout, getAllPayouts };
+module.exports = {
+  addWorkerPayout,
+  addSitePayout,
+  getAllPayouts,
+  getWorkerPayouts,
+  getSitePayouts,
+};
--- router/payout.js
+++ router/payout.js
@@ -2,8 +2,13 @@ const Payout = require("../controller/payout.controller");
 const { verifyAdminAndSupervisor } = require("../middlewares/verifyToken");
 const router = require("express").Router();
 
+// post
 router.post("/worker", verifyAdminAndSupervisor, Payout.addWorkerPayout);
 router.post("/site", verifyAdminAndSupervisor, Payout.addSitePayout);
-router.post("/all", verifyAdminAndSupervisor, Payout.getAllPayouts);
+
+// get
+router.get("/worker", verifyAdminAndSupervisor, Payout.getWorkerPayouts);
+router.get("/site", verifyAdminAndSupervisor, Payout.getSitePayouts);
+router.get("/all", verifyAdminAndSupervisor, Payout.getAllPayouts);
 
 module.exports = router;
