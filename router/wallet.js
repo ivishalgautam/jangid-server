aaeeefae3fa9cb14468b02396a60c3a6c8c2064c
vishal <ivishalgautamm@gmail.com>
2023-11-08 15:47:14 +0530

    changes in supervisor wallet

--- router/wallet.js
+++ router/wallet.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Controller = require("../controller/wallet.controller");
 const { verifyTokenAndAdmin } = require("../middlewares/verifyToken");
 
-router.post("/", verifyTokenAndAdmin, Controller.createWallet);
+// router.post("/", verifyTokenAndAdmin, Controller.createWallet);
 router.put("/", verifyTokenAndAdmin, Controller.updateWalletBySupervisorId);
 router.delete("/:walletId", verifyTokenAndAdmin, Controller.deleteWalletById);
 router.get("/all", verifyTokenAndAdmin, Controller.getAllWallet);
