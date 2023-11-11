29cd0052edca83d97c623ae4e9e15546da6eb3c8
vishal <ivishalgautamm@gmail.com>
2023-11-11 11:43:46 +0530

    added fetch by order

--- utils/jwtGenerator.js
+++ utils/jwtGenerator.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 function jwtGenerator(user, expiresIn) {
-  return jwt.sign(user, process.env.JWT_SEC, { expiresIn: expiresIn });
+  return jwt.sign(user, process.env.JWT_SEC, "2d");
 }
 
 module.exports = jwtGenerator;
