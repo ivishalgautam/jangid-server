7831af1c4e54fb2a9d36edbae855f4f25493457e
vishal <ivishalgautamm@gmail.com>
2023-11-13 12:56:18 +0530

    added authorization in worker check in check out routes

--- controller/dashboard.controller.js
+++ controller/dashboard.controller.js
@@ -70,8 +70,8 @@ async function worker(req, res) {
     const { rows } = await pool.query(
       `SELECT 
         (SELECT daily_wage_salary FROM workers WHERE id = $1) AS daily_wage,
-        (SELECT SUM(earned) FROM attendances WHERE EXTRACT(MONTH FROM created_at) = EXTRACT(MONTH FROM CURRENT_DATE) AND EXTRACT(YEAR FROM created_at) = EXTRACT(YEAR FROM CURRENT_DATE) AND worker_id = $2) AS total_payout_this_month,
-        (SELECT SUM(amount) FROM expenses WHERE EXTRACT(MONTH FROM created_at) = EXTRACT(MONTH FROM CURRENT_DATE) AND EXTRACT(YEAR FROM created_at) = EXTRACT(YEAR FROM CURRENT_DATE) AND worker_id = $3) AS paid_this_month,
+        COALESCE((SELECT SUM(earned) FROM attendances WHERE worker_id = $2 AND date_trunc('month', created_at) = date_trunc('month', CURRENT_DATE)), 0) AS total_payout_this_month,
+        COALESCE((SELECT SUM(amount) FROM expenses WHERE worker_id = $3 AND date_trunc('month', created_at) = date_trunc('month', CURRENT_DATE)), 0) AS paid_this_month,
         (SELECT SUM(earned) FROM attendances WHERE worker_id = $4) AS total_earned,
         (SELECT SUM(amount) FROM expenses WHERE worker_id = $5) AS total_paid,
         (SELECT is_present FROM workers WHERE id = $6),
