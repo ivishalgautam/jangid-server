79d3f129c75af9eb362b9a0c1f81b24183d506ce
vishal <ivishalgautamm@gmail.com>
2023-11-13 16:16:51 +0530

    added authorization in worker check in check out routes

--- utils/convertMillisecondsToTime.js
+++ utils/convertMillisecondsToTime.js
@@ -1,7 +1,9 @@
 function convertMillisecondsToTime(milliseconds) {
-  return `${new Date(milliseconds).getHours()}hours ${new Date(
-    milliseconds
-  ).getMinutes()}minutes ${new Date(milliseconds).getSeconds()}seconds`;
+  var seconds = Math.floor((milliseconds / 1000) % 60);
+  var minutes = Math.floor((milliseconds / (1000 * 60)) % 60);
+  var hours = Math.floor((milliseconds / (1000 * 60 * 60)) % 24);
+
+  return `${hours} hours ${minutes} minutes ${seconds} seconds`;
 }
 
 module.exports = { convertMillisecondsToTime };
