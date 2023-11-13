05b3e175f47f051b3fba57c38ef68fe6b78d749b
vishal <ivishalgautamm@gmail.com>
2023-11-13 11:37:02 +0530

    added new changes in worker dashboard

--- controller/dashboard.controller.js
+++ controller/dashboard.controller.js
@@ -69,7 +69,7 @@ async function worker(req, res) {
     // (SELECT SUM(hours) FROM attendances WHERE EXTRACT(MONTH FROM created_at) = EXTRACT(MONTH FROM CURRENT_DATE) AND EXTRACT(YEAR FROM created_at) = EXTRACT(YEAR FROM CURRENT_DATE) AND worker_id = $2) AS total_work_this_month
     const { rows } = await pool.query(
       `SELECT 
-        (SELECT daily_wage_salary FROM workers WHERE worker_id = $1) AS daily_wage,
+        (SELECT daily_wage_salary FROM workers WHERE id = $1) AS daily_wage,
         (SELECT SUM(earned) FROM attendances WHERE EXTRACT(MONTH FROM created_at) = EXTRACT(MONTH FROM CURRENT_DATE) AND EXTRACT(YEAR FROM created_at) = EXTRACT(YEAR FROM CURRENT_DATE) AND worker_id = $2) AS total_payout_this_month,
         (SELECT SUM(amount) FROM expenses WHERE EXTRACT(MONTH FROM created_at) = EXTRACT(MONTH FROM CURRENT_DATE) AND EXTRACT(YEAR FROM created_at) = EXTRACT(YEAR FROM CURRENT_DATE) AND worker_id = $3) AS paid_this_month,
         (SELECT SUM(earned) FROM attendances WHERE worker_id = $4) AS total_earned,
