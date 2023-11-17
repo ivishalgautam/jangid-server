b57376c5178e1c60741082f47b0c2cb6530f1773
vishal <ivishalgautamm@gmail.com>
2023-11-17 13:26:56 +0530

    added file delete files routes

--- controller/file.controller.js
+++ controller/file.controller.js
@@ -5,15 +5,20 @@ async function deleteFile(req, res) {
   try {
     const { filename } = req.params;
     const file = path.join(__dirname, "../assets/images", filename);
-    fs.unlink(file, (err) => {
-      if (err) {
-        console.log(
-          `error deleting file: ${file} message:${JSON.stringify(err)}`
-        );
-      } else {
-        console.log("file deleted");
-      }
-    });
+
+    if (fs.existsSync(file)) {
+      fs.unlinkSync(file, (err) => {
+        if (err) {
+          console.log(
+            `error deleting file: ${file} message:${JSON.stringify(err)}`
+          );
+        } else {
+          console.log("file deleted");
+        }
+      });
+    } else {
+      return res.status(404).json({ message: "file not found!" });
+    }
 
     res.json({ message: "file deleted" });
   } catch (error) {
