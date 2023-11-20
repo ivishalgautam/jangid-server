205a20acc7d89f38bb76d0da88ffd3beac47e019
vishal <ivishalgautamm@gmail.com>
2023-11-20 13:09:13 +0530

    added punch out for admin

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -268,14 +268,19 @@ async function workerCheckOut(req, res) {
     }
 
     if (req.user.role === "admin") {
-      const d = punch_out_time.split(" ")[0].split("/").join("-");
+      const d = punch_out_time.split(" ")[0];
+      const yyyy = d.split("/")[2];
+      const mm = d.split("/")[1];
+      const dd = d.split("/")[0];
+      // const d = punch_out_time.split(" ")[0].split("/").join("-");
       const t = punch_out_time.split(" ")[1];
-      console.log(`${d + "T" + t}`);
+
+      console.log(`${yyyy}-${mm}-${dd}`);
       console.log(new Date(`${d + "T" + t}`));
       data = await pool.query(
         `UPDATE check_in_out set check_out = $1 WHERE uid = $2 returning *`,
         [
-          new Date(`${d + "T" + t}`)
+          new Date(`${yyyy}-${mm}-${dd}`)
             .toISOString()
             .slice(0, 19)
             .replace("T", " "),
